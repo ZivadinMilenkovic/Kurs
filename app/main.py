74396fd820d4ab99a0c220cc6dc33a0119e0129c
ZivadinMilenkovic <zivadin.milenkovic@dev.telnet.rs>
2024-03-15 12:51:24 +0100

    Enable usage of our API on other websites

--- app/main.py
+++ app/main.py
@@ -2,12 +2,23 @@ from fastapi import FastAPI
 from . import model
 from .database import engine, SessionLocal
 from .routers import user, post, auth, vote
-
+from fastapi.middleware.cors import CORSMiddleware
 # makes new table that we define in model
 
 # model.Base.metadata.create_all(bind=engine)
 
 # declaring type of crypting algorithm
+app = FastAPI()
+
+origins = ["https://www.google.com"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 app = FastAPI()
 
